8f21a9f204fb075e365dbcdc797c61d28f13fdf5
Erkamck <34324019+Erkamck@users.noreply.github.com>
2017-12-06 18:34:24 -0800

    Add files via upload
    I couldn't figure what's wrong my code out.

---
+++ Transmitter_lcd/Transmitter_lcd.ino
@@ -0,0 +1,43 @@
+#include <LiquidCrystal.h>
+
+
+
+#include <VirtualWire.h>
+
+#include <Wire.h>
+
+
+#define txPowerPin 8
+char msg[3];
+char msg1[3];
+void setup()
+{
+    Serial.begin(9600);    // Debugging only
+    Serial.println("setup");
+
+    // Initialise the IO and ISR
+   // vw_set_ptt_inverted(true); // Required for DR3100
+    
+  pinMode(txPowerPin, OUTPUT);
+  pinMode(txPowerPin, LOW);
+  vw_setup(4800);     // VirtualWire communication speed
+  vw_set_tx_pin(9);
+}
+
+void loop()
+{
+    const char *msg = "X";
+    digitalWrite(txPowerPin, HIGH);
+ 
+    strcat(msg, msg1);
+    vw_send((uint8_t *)msg, strlen(msg));
+    vw_wait_tx(); // Wait until the whole message is gone
+    
+    delay(700);
+   digitalWrite(txPowerPin, LOW);
+ 
+   delay(5000); 
+  
+ 
+    
+}
---
+++ receiver_lcd/receiver_lcd.ino
@@ -0,0 +1,49 @@
+
+
+#include <LiquidCrystal.h>
+
+#include <VirtualWire.h>
+
+#include <Wire.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  // Set the LCD I2C address
+#define rx 2
+int i;
+void setup()
+{
+    Serial.begin(9600);  // Debugging only
+     lcd.begin(16,2); 
+    lcd.print("setup");
+
+    // Initialise the IO and ISR
+    //vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(4800);  // Bits per sec
+
+    vw_rx_start();
+    vw_set_rx_pin(2);// Start the receiver PLL running
+    lcd.clear();       
+}
+
+void loop()
+{
+    
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+      lcd.print((char *)buf);
+      lcd.write(buf[3]);
+      lcd.setCursor(0,1);
+      lcd.print("X");//was char *
+      delay(5000); //was 5000
+      lcd.clear();
+    }
+   else if (!vw_get_message(buf, &buflen))
+   {
+     lcd.print("Out of range.");
+     delay(5000);
+     lcd.clear();
+   }
+}
+
